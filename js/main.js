71c6062fae76da31894dca88530a2e7b863627f9
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-02-22 09:29:55 +0200

    error fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(function(){
-<<<<<<< HEAD
-=======
 $('.banner-section__slider').slick({
 dots: true,
   infinite: true,
@@ -10,6 +8,5 @@ arrows: true,
 prevArrow:'<button class="banner-section__slider-btn banner-section__slider-btnPrev"><img src="images/arrow-prev.svg">',
 nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btnNext"><img src="images/arrow-next.svg">'
 });
->>>>>>> 860cf222af1c8681ad41e4a3ac73709a8ae18367
 
 });
