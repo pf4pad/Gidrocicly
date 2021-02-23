438c55c3d106bdfa461ff7962300b89b2f040eac
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-02-23 22:59:59 +0200

    tabs

--- js/main.js
+++ js/main.js
@@ -9,4 +9,13 @@ prevArrow:'<button class="banner-section__slider-btn banner-section__slider-btnP
 nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btnNext"><img src="images/arrow-next.svg">'
 });
 
+
+$('.search__tabs-item').on('click', function(e){
+  e.preventDefault();
+
+  $('.search__tabs-item').removeClass('search__tabs-item--active');
+  $('.search__content-item').removeClass('search__content-item--active');
+  $(this).addClass('search__tabs-item--active');
+$($(this).attr('href')).addClass('search__content-item--active')
+  });
 });
