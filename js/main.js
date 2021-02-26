59aa8d48777744c0fe7e6a16ecc171e7f73e7ead
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-02-26 02:49:56 +0200

    footer

--- js/main.js
+++ js/main.js
@@ -13,23 +13,24 @@ nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btnN
 $('.tab').on('click', function(e){
   e.preventDefault();
 
-  $('.tab').removeClass('tab--active');
-  $('.tabs-content').removeClass('tabs-content--active');
+  $($(this).siblings()).removeClass('tab--active');
+  $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
   $(this).addClass('tab--active');
 $($(this).attr('href')).addClass('tabs-content--active')
   });
 
   $('.product-item__favorite').on('click', function(){
-    $('.product-item__favorite').toggleClass('product-item__favorite--active')
+    $(this).toggleClass('product-item__favorite--active')
   });
 
   $('.product-slider').slick({
-infinite: true,
+    dots: false,
+  infinite: true,
   slidesToShow: 4,
   slidesToScroll: 1,
   arrows: true,
-prevArrow:'<button class="banner-section__slider-btn banner-section__slider-btnPrev"><img src="images/products/arrow-black-prev.svg">',
-nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btnNext"><img src="images/products/arrow-black-next.svg">'
+prevArrow:'<button class="product-slider__slider-btn product-slider__slider-btnPrev"><img src="images/products/arrow-black-prev.svg">',
+nextArrow:'<button class="product-slider__slider-btn product-slider__slider-btnNext"><img src="images/products/arrow-black-next.svg">'
 
   });
 });
